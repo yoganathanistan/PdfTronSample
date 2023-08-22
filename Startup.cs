97ff97fb2e24d0e76ccee1db597b183a35f3b4c7
Anistan Yoganath <104888344+yoganathanistan@users.noreply.github.com>
2023-08-22 12:35:34 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿namespace PdfTronSample
+{
+    public class Startup
+    {
+        
+        private const string AllowAnyOrigins = "AllowAnyOrigin";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c =>
+            {
+                c.AddPolicy(AllowAnyOrigins, options => options.AllowAnyOrigin().AllowAnyHeader());
+            });
+            services.AddControllers();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "Root";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseCors(AllowAnyOrigins);
+            app.UseSpaStaticFiles();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "Root";
+            });
+        }
+    }
+}
